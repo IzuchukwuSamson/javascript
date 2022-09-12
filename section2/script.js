86e0351a3c645e7e7d8c0960699a4b277ade9dcb
IzuchukwuSamson <izuchukwusamson5@gmail.com>
2022-09-12 22:51:08 +0100

    updates

--- section2/script.js
+++ section2/script.js
@@ -28,3 +28,53 @@ console.log(BMIMark > BMIJohn);
 // 5 falsy values: 0, '', undefined, null, NaN
 // falsy values will be converted to false when used as a Boolean
 // Aside these values, every other value is a truthy value
+
+// SWITCH STATEMENT
+// switch statement was designed for equality
+// unlike if/else statement that was desined for comparison
+const day = 'friday';
+switch (day) {
+  case 'monday':
+    console.log('A');
+    console.log('A1');
+    break;
+  case 'tuesday':
+    console.log('B');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('C');
+    break;
+  case 'friday':
+    console.log('D');
+    break;
+  case 'saturday':
+    console.log('E');
+    break;
+  case 'sunday':
+    console.log('F');
+    break;
+
+  default:
+    console.log('G-Z');
+    break;
+}
+
+const date = 'monday';
+if (date === 'monday') {
+  console.log('A');
+} else if (date === 'tuesday') {
+  console.log('B');
+} else {
+  console.log('G-Z');
+}
+
+// Statement and expressions
+3 + 4; //is an expression
+1998; //is an expression
+
+true && false && !false; //are statements
+// if/else are also statements
+if (23 > 10) {
+  const str = '23 is bigger'; //a statement
+}
