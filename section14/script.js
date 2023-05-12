4212bae29b7e2ae85c8b860b0f80f30ae5103ca6
Izuchukwu Samson <izuchukwusamson5@gmail.com>
2023-05-12 08:12:23 +0100

    section 14

--- section14/script.js
+++ section14/script.js
@@ -14,6 +14,23 @@ const izu = new Person('Izu', 1997);
 const mary = new Person('mary', 1998);
 const jude = new Person('jude', 1999);
 const kent = new Person('kent', 2000);
-// console.log(izu, mary, jude, kent);
+console.log(izu, mary, jude, kent);
 
-console.log(jude instanceof Person);
+// console.log(jude instanceof Person);
+
+//Prototypes
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+console.log(Person.prototype);
+izu.calcAge();
+mary.calcAge();
+console.log(izu.__proto__);
+console.log(izu.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(izu.species);
+
+console.log(izu.hasOwnProperty('firstName'));
--- section14/script.ts
+++ section14/script.ts
@@ -5,8 +5,17 @@ const ThePerson = function (firstName: string, birthYear: number): void {
   this.birthYear = birthYear;
 };
 
-// const Izu = new ThePerson('Izu', 1997);
+const Izu = new ThePerson('Izu', 1997);
 // console.log(Izu);
+
+// console.log(Izu instanceof Person);
 
 // let myName: ThePerson = new ThePerson('Izu', 1997)
 // let car: Person = new Car();
+
+// Protoype
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+Izu.calcAge();
