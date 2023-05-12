fb7a72736a492b3b0cfd3b4cb2f3d932919a1d5f
Izuchukwu Samson <izuchukwusamson5@gmail.com>
2023-05-12 07:33:18 +0100

    section 14

--- section14/script.js
+++ section14/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const izu = new Person('Izu', 1997);
+const mary = new Person('mary', 1998);
+const jude = new Person('jude', 1999);
+const kent = new Person('kent', 2000);
+// console.log(izu, mary, jude, kent);
+
+console.log(jude instanceof Person);
---
+++ section14/script.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+const ThePerson = function (firstName: string, birthYear: number): void {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+// const Izu = new ThePerson('Izu', 1997);
+// console.log(Izu);
+
+// let myName: ThePerson = new ThePerson('Izu', 1997)
+// let car: Person = new Car();
