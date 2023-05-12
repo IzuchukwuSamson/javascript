aebcdf5688e0ae4c9baaa99c8ed34be10a729209
Izuchukwu Samson <izuchukwusamson5@gmail.com>
2023-05-12 21:40:43 +0100

    section 14

--- section14/script.js
+++ section14/script.js
@@ -1,36 +1,93 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-  // Never do this
-  // this.calcAge = function () {
-  //   console.log(2037 - this.birthYear);
-  // };
-};
+//   // Never do this
+//   // this.calcAge = function () {
+//   //   console.log(2037 - this.birthYear);
+//   // };
+// };
 
-const jane = new Person('jane', 1990);
-const mary = new Person('mary', 1998);
-const jude = new Person('jude', 1999);
-const kent = new Person('kent', 2000);
-console.log(jane, mary, jude, kent);
+// const jane = new Person('jane', 1990);
+// const mary = new Person('mary', 1998);
+// const jude = new Person('jude', 1999);
+// const kent = new Person('kent', 2000);
+// console.log(jane, mary, jude, kent);
 
 // console.log(jude instanceof Person);
 
 //Prototypes
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
+
+// console.log(Person.prototype);
+// jane.calcAge();
+// mary.calcAge();
+// console.log(jane.__proto__);
+// console.log(jane.__proto__ === Person.prototype);
+// console.log(Person.prototype.isPrototypeOf(Person));
+
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(jane.species);
+
+// console.log(jane.hasOwnProperty('firstName'));
+
+// ES6 Syntax
+// class expresssion
+// const PersonCl = class Person {};
+class Person {
+  constructor(firstname, birthYear) {
+    this.firstname = firstname;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  get age() {
+    return 2039 - this.birthYear;
+  }
+  set fullName(name) {
+    // return `${this.firstname} ${this.birthYear}`
+    if (name.includes(' ')) {
+      this.fullName = name;
+    }
+  }
+}
+
+const jessica = new Person('jessisa', 1996);
+jessica.calcAge();
+console.log(jessica.__proto__ === Person.prototype);
+
+Person.prototype.greet = function () {
+  console.log('Hey there!' + this.firstname);
 };
 
-console.log(Person.prototype);
-jane.calcAge();
-mary.calcAge();
-console.log(jane.__proto__);
-console.log(jane.__proto__ === Person.prototype);
-console.log(Person.prototype.isPrototypeOf(Person));
+jessica.greet();
+console.log(jessica.age);
 
-Person.prototype.species = 'Homo Sapiens';
-console.log(jane.species);
+// GETTEERS
+const account = {
+  owner: 'Jane',
+  movements: [200, 300, 400, 500, 600],
 
-console.log(jane.hasOwnProperty('firstName'));
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(move) {
+    this.movements.push(move);
+  },
+
+  get age() {
+    return;
+  },
+};
+
+account.latest = 50;
+
+console.log(account.movements);
--- section14/script.ts
+++ section14/script.ts
@@ -1,11 +1,11 @@
 'use strict';
 
-const ThePerson = function (firstName: string, birthYear: number): void {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
-};
+// const ThePerson = function (firstName: string, year: number): void {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
 
-const sam = new ThePerson('sam', 1990);
+// const sam = new ThePerson('sam', 1990);
 // console.log(sam);
 
 // console.log(sam instanceof ThePerson);
@@ -14,18 +14,38 @@ const sam = new ThePerson('sam', 1990);
 // let car: Person = new Car();
 
 //Prototypes
-ThePerson.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
+// ThePerson.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
 
-console.log(ThePerson.prototype);
-sam.calcAge();
-mary.calcAge();
-console.log(sam.__proto__);
-console.log(sam.__proto__ === ThePerson.prototype);
-console.log(ThePerson.prototype.isPrototypeOf(ThePerson));
+// console.log(ThePerson.prototype);
+// sam.calcAge();
+// mary.calcAge();
+// console.log(sam.__proto__);
+// console.log(sam.__proto__ === ThePerson.prototype);
+// console.log(ThePerson.prototype.isPrototypeOf(ThePerson));
 
-ThePerson.prototype.species = 'Homo Sapiens';
-console.log(sam.species);
+// ThePerson.prototype.species = 'Homo Sapiens';
+// console.log(sam.species);
 
-console.log(sam.hasOwnProperty('firstName'));
+// console.log(sam.hasOwnProperty('firstName'));
+
+// ES6 Syntax
+// class expresssion
+// const PersonCl = class Person {};
+
+class ThePerson {
+  public name: string = 'Sam';
+  public year: number = 20;
+
+  constructor(firstname: string, birthYear: number) {
+    this.name = firstname;
+    this.year = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.year);
+  }
+}
+
+const felix = new Person('felix', 1996);
