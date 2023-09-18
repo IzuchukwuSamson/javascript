e6b0cbadb311692f5817855d91e57ab318ac7e93
Izuchukwu Samson <izuchukwusamson5@gmail.com>
2023-09-18 05:21:31 +0100

    updates

---
+++ practice/game.js
@@ -0,0 +1,12 @@
+// function canExecuteFastAttack() {}
+function processBooleanValue(boolValue) {
+  if (boolValue === true) {
+    console.log('The boolean value is true.');
+  } else {
+    console.log('The boolean value is false.');
+  }
+}
+
+// Call the function with a boolean value
+processBooleanValue(true); // Output: The boolean value is true.
+processBooleanValue(false); // Output: The boolean value is false.
