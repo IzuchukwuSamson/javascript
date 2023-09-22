20d1ba825e2c63a9ed8f3f8f4a63c83a18072520
Izuchukwu Samson <izuchukwusamson5@gmail.com>
2023-09-22 06:56:26 +0100

    updates

---
+++ practice/classes.js
@@ -0,0 +1,11 @@
+// const bigDay = new Date(2024, 6, 19);
+// console.log(bigDay.toLocaleDateString());
+// if (bigDay.getTime() < Date.now()) {
+//   console.log('Once upon a time...');
+// } else {
+//   console.log('Sometime in the future');
+// }
+
+class myClass {
+  constructor() {}
+}
